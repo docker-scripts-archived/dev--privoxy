d8ca6062e41fdc997f704f9ea509f0696ec1501a
Sahil Khan <sahil@null.co.in>
2018-03-09 14:37:08 +0530

    Create privoxy-start.sh

---
+++ scripts/privoxy-start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+CONFFILE=/etc/privoxy/config
+PIDFILE=/var/run/privoxy.pid
+
+chmod +r /etc/privoxy/config && chmod +x /usr/local/bin/privoxy-start.sh
+
+if [ ! -f "${CONFFILE}" ]; then
+        echo "Configuration file ${CONFFILE} not found!"
+        exit 1
+fi
+
+/usr/sbin/privoxy --no-daemon --pidfile "${PIDFILE}" "${CONFFILE}"
